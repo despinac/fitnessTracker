170ffa05041fa623daae7bd51038055d4421b5cd
Despina Constantinidou <deco00001@stud.uni-saarland.de>
2024-07-18 19:54:34 +0200

    frist try with data

---
+++ script.js
@@ -0,0 +1,27 @@
+d3.csv("data.csv").then(function (data)) {
+var trainings = data;
+var button = d3.select("#button");
+var form = d3.select("#form");
+button.on("click", runEnter);
+form.on("submit", runEnter);
+}
+
+function runEnter() {
+
+d3.select("tbody").html("");
+
+d3.event.preventDefault();
+
+var inputValue = d3.select("#user-input").property("value");
+
+var filteredTrainings = trainings.filter(trainings => trainings.vorname.includes(inputValue));
+
+for (var i = 0; i < filteredTrainings.length; i++) {
+
+d3.select("tbody").insert("tr").html(
+"<td>" + (output[i]['nummer'])+"</td>" +
+"<td>" + (output[i]['vorname'])+"</td>" +
+"<td>" + (output[i]['datum'])+"</td>" +
+)
+}
+}
